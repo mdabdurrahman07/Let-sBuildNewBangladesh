3fcec8b4d4de98fe7bf3c3438c21ce96a576a822
mdabdurrahman07 <jamilrahman2001@gmail.com>
2024-08-18 19:18:49 +0600

    packages installed and readme.md file updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 /* eslint-disable react/no-unescaped-entities */
-import Image from "next/image";
+
+
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-center text-3xl">Let'sBuildNewBangladesh</h1>
+      
     </div>
   );
 }
